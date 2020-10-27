fed7776314f8d1a1d1d3d708128c930241856ac2
unknown <ziratae@outlook.com>
2020-10-27 12:27:37 -0400

    More

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-const fs = require("fs");
-const Discord = require("discord.js");
-const config = require("./config.json");
+const path = require("path");
+const Weeg = require("./src/Weeg.js");
+const client = new Weeg();
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-// Populate commands collection
-fs.readdirSync("./src/commands").filter((file) => {
-  return file.endsWith(".js");
-}).forEach((file) => {
-  let command = require(`./src/commands/${file}`);
-  client.commands.set(command.name, command);
-});
-
-const prefix = "!";
-
-client.on("message", (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) {
-    console.log("Return");
-    return;
-  }
-
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  console.log(args, command);
-
-  client.commands.get(command).execute(message, args);
-
-  /*
-  if (command === "ping") {
-    client.commands.get("ping").execute(message, args);
-  }
-  */
-});
-
-client.once("ready", () => {
-  console.log("Online");
-});
-
-client.login(process.env.TOKEN || config.WEEG.TOKEN);
+client.loadCommands(path.join(__dirname, "src", "commands"));
+client.login(client.config.TOKEN);
