fe287bfd212a8e60451d7229a7f141f179f2b4ce
unknown <ziratae@outlook.com>
2020-10-27 08:42:40 -0400

    More changes

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 
+const prefix = "!";
+
 client.once("ready", () => {
   console.log("Online");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) {
+    return;
+  }
+
+  const args = message.content.slice(prefix).split(/ +/);
+  const command = args.shift().toLowerCase();
 });
 
 client.login(process.env.TOKEN || config.WEEG.TOKEN);
