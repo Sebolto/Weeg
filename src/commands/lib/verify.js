af505980db5ef54aaf9e2c2c8c8960e2d48f2064
unknown <ziratae@outlook.com>
2020-10-28 06:16:53 -0400

    +dotenv

--- src/commands/lib/verify.js
+++ src/commands/lib/verify.js
@@ -3,8 +3,34 @@ const lang = require("../../resources/lang.json");
 
 class Verify extends Command {
 
+  getEditCountAndID (username) {
+    return got("https://dev.fandom.com/api.php", {
+      searchParams: {
+        action: "query",
+        list: "users",
+        usprop: "editcount",
+        ususers: username,
+        format: "json"
+      }
+    }).json();
+  }
+
+  getMastheadDiscord (userid) {
+    return got("https://services.fandom.com/user-attribute/user/" +
+        userid + "/attr/discordHandle", {
+      headers: {
+        accept: "*/*"
+      }
+    }).json();
+  }
+
+  //770419446278914108
   execute (message, args) {
-    message.channel.send("verify yourself!");
+    if (message.member.roles.cache.has("770419446278914108")) {
+      message.channel.send("Already have this role");
+    } else {
+      message.channel.send("You don't have this role");
+    }
   }
 }
 
